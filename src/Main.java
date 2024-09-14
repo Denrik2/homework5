47b96e6524bfe637a593312e2b51fc077f63b38e
Denrik <ketovdany@yandex.ru>
2024-09-14 22:08:12 +0400

    дз5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args){
+        //Задание №1.
+        int clientOS = 1;
+        System.out.println("Задание 1");
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else {
+            System.out.println("Мы не знаем что это за операционная система");
+        }
+
+        //Задание №2.
 
     }
 }
