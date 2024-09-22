36b35414d57f407fdaea0cdf66515550267313f9
Denrik <ketovdany@yandex.ru>
2024-09-22 17:56:50 +0400

    дз5

--- src/Main.java
+++ src/Main.java
@@ -12,25 +12,23 @@ public class Main {
         }
 
         //Задание №2.
-        int yearOfRelease = 2016;
+        int clientDeviceYear = 2014;
         System.out.println("Задание №2");
-        if (clientOS == 1) {
-            if (yearOfRelease > 2015)
-                System.out.println("Установите версию приложения для Android по ссылке");
-            else System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientOS == 0) {
-            if (yearOfRelease > 2015)
-                System.out.println("Установите версию приложения для Ios по ссылке");
-            else System.out.println("Установите версию приложения для IOS по ссылке");
-        } else {
-            System.out.println("Мы не знаем что это за операционная система");
+        if (clientOS == 1 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } if (clientOS == 0 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
         }
 
         //Задание №3.
         int year = 2024;
         System.out.println("Задание №3");
         if (year > 1584) {
-            if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
                 System.out.println(year + " год является високостным");
             } else System.out.println(year + " год не является високостным");
         } else System.out.println("Год не может быть меньше 1584");
@@ -47,9 +45,9 @@ public class Main {
         }else System.out.println("Доставки нет(");
 
         //Задание №5.
-        int month = 6;
+        int monthNumber = 6;
         System.out.println("Задание №5");
-        switch (month) {
+        switch (monthNumber) {
             case 1, 2, 12 -> System.out.println("Зима");
             case 3, 4, 5 -> System.out.println("Весна");
             case 6, 7, 8 -> System.out.println("Лето");
